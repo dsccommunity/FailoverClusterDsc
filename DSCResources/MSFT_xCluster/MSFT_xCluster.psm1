5ced3f7e1f9cffe3ce4f2bd0d11c945cc70167ab
tragiccode <michael@tragiccode.com>
2017-09-09 11:45:27 -0500

    Remove double space after period

--- DSCResources/MSFT_xCluster/MSFT_xCluster.psm1
+++ DSCResources/MSFT_xCluster/MSFT_xCluster.psm1
@@ -14,7 +14,7 @@ $script:localizedData = Get-LocalizedData -ResourceName 'MSFT_xCluster'
         Static IP Address of the failover cluster.
 
     .PARAMETER IgnoreNetwork
-        One or more networks to ignore when creating the cluster.  Typically
+        One or more networks to ignore when creating the cluster. Typically
         used to ignore networks with DHCP enabled since they are always included
         by default.
 
@@ -94,7 +94,7 @@ function Get-TargetResource
         Static IP Address of the failover cluster.
 
     .PARAMETER IgnoreNetwork
-        One or more networks to ignore when creating the cluster.  Typically
+        One or more networks to ignore when creating the cluster. Typically
         used to ignore networks with DHCP enabled since they are always included
         by default.
 
@@ -242,7 +242,7 @@ function Set-TargetResource
         Not used in Test-TargetResource.
 
     .PARAMETER IgnoreNetwork
-        One or more networks to ignore when creating the cluster.  Typically
+        One or more networks to ignore when creating the cluster. Typically
         used to ignore networks with DHCP enabled since they are always included
         by default.
 

9d9880850095c5c7d42807b15c8b08d99966bed1
Johan Ljunggren <johan.ljunggren@shamn.se>
2019-12-25 08:23:25 +0100

    Update published example

--- source/Examples/Resources/xCluster/1-CreateFirstNodeOfAFailoverCluster.ps1
+++ source/Examples/Resources/xCluster/1-CreateFirstNodeOfAFailoverCluster.ps1
@@ -1,6 +1,42 @@
+<#PSScriptInfo
+
+.VERSION 1.0.1
+
+.GUID 2a4174f6-aa62-49c8-bee3-a288f70ebcfc
+
+.AUTHOR DSC Community
+
+.COMPANYNAME DSC Community
+
+.COPYRIGHT DSC Community contributors. All rights reserved.
+
+.TAGS DSCConfiguration
+
+.LICENSEURI https://github.com/dsccommunity/xFailOverCluster/blob/master/LICENSE
+
+.PROJECTURI https://github.com/dsccommunity/xFailOverCluster
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+Updated author and copyright notice.
+
+.PRIVATEDATA 2016-Datacenter,2016-Datacenter-Server-Core
+
+#>
+
+#Requires -Module xFailOverCluster
+
+
 <#
-.EXAMPLE
-    This example shows how to create the failover cluster on the first node.
+    .DESCRIPTION
+        This example shows how to create the failover cluster on the first node.
 #>
 
 Configuration Example

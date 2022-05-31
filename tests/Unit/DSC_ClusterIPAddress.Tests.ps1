d722e12ad16221befab6913808cf4e68d75fb3ce
Nick Germany <nickgw@outlook.com>
2022-05-31 12:30:15 -0400

    First unit tests

--- tests/Unit/DSC_ClusterIPAddress.Tests.ps1
+++ tests/Unit/DSC_ClusterIPAddress.Tests.ps1
@@ -29,6 +29,71 @@ function Invoke-TestSetup
     $global:moduleVersion = $ModuleVersion
 }
 
+Describe "$script:dscModuleName\Get-TargetResource" {
+
+    Mock -CommandName Test-IPAddress -MockWith {
+        return $True
+    }
+
+    Context 'When the system is in the desired state' {
+
+        $mockTestParmaters = @{
+            Ensure      = 'Present'
+            Address     = '192.168.1.41'
+            AddressMask = '255.255.255.0'
+        }
+        Context 'When Ensure is set to ''Present'' but the IP Address is not added to the cluster' {
+
+            Mock -CommandName Get-ClusterResource -MockWith {
+                return @{}
+            }
+
+            Mock -CommandName Get-ClusterIPResource -MockWith {
+                return @{}
+            }
+
+            It 'Should return an empty hashtable' {
+                Get-TargetResource @mockTestParameters | Should -Be @{}
+            }
+        }
+
+        Context 'When Ensure is set to ''Present'' and the IP Address is added to the cluster' {
+            $correctResult = @{
+                IPAddress   = $mockTestParameters.Address
+                AddressMask = $mockTestParameters.AddressMask
+                Ensure      = 'Present'
+            }
+
+            Mock -CommandName Get-ClusterResource -MockWith {
+                return @{
+                    Name = "IP Address $($mockTestParameters.Address)"
+                    State = 'Online'
+                    OnwerGroup = 'Cluster Group'
+                    ResourceType = 'IP Address'
+                }
+            }
+
+            Mock -CommandName Get-ClusterIPResource -MockWith {
+                return @{
+                    Address     = $mockTestParameters.Address
+                    AddressMask = $mockTestParameters.AddressMask
+                    Network     = '192.168.1.1'
+                }
+            }
+            It 'Should return the correct hashtable' {
+                Get-TargetResource @mockTestParameters | Should -Be $correctResult
+            }
+        }
+    }
+}
+
+Describe "$script:dscModuleName\Set-TargetResource" {
+}
+
+Describe "$script:dscModuleName\Test-TargetResource" {
+}
+
+
 function Invoke-TestCleanup
 {
     Restore-TestEnvironment -TestEnvironment $script:testEnvironment

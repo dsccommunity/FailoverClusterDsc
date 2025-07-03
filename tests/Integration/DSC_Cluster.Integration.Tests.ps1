c982c9f412c90bd66ecd247a1a8a2586b6a3f02f
Dan Hughes <2237515+dan-hughes@users.noreply.github.com>
2025-07-03 18:46:28 +0100

    Add dummy integration test

---
+++ tests/Integration/DSC_Cluster.Integration.Tests.ps1
@@ -0,0 +1,51 @@
+<#
+    .SYNOPSIS
+        Integration test for DSC_Cluster DSC resource.
+
+    .NOTES
+#>
+
+# Suppressing this rule because Script Analyzer does not understand Pester's syntax.
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param ()
+
+BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
+            }
+
+            # This will throw an error if the dependencies have not been resolved.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
+    }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
+}
+
+BeforeAll {
+    $script:dscModuleName = 'FailoverClusterDsc'
+    $script:dscResourceName = 'DSC_Cluster'
+
+    $script:testEnvironment = Initialize-TestEnvironment `
+        -DSCModuleName $script:dscModuleName `
+        -DSCResourceName $script:dscResourceName `
+        -ResourceType 'Mof' `
+        -TestType 'Integration'
+}
+
+AfterAll {
+    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+}
+
+Describe "$($script:dscResourceName)_Integration" -Skip:$true {
+    return
+}
